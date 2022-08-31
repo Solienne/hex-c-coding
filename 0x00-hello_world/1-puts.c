645b82a3f1ef4e9616954594843a34b3cd28a9dc
Solienne <soliene.teixeira@ufba.br>
2022-08-31 13:51:39 +0000

    puts

--- 0x00-hello_world/1-puts.c
+++ 0x00-hello_world/1-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-/**
-*main - entry points
-*return: always 0, success
-*/
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ *
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
