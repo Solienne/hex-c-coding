79a6046d13110f44036a0a07f0fd05e47e6acede
Solienne <soliene.teixeira@ufba.br>
2022-08-31 13:38:29 +0000

    printing with puts

--- 0x00-hello_world/1-puts.c
+++ 0x00-hello_world/1-puts.c
@@ -1,10 +1,11 @@
-/**
- *main - entry points
- *return: always 0, success
- */
 #include <stdio.h>
+/**
+*main - entry points
+*return: always 0, success
+*/
+
 int main(void)
 {
-  puts ("\"Programming is like building a multilingual puzzle");
-	return (0); 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
