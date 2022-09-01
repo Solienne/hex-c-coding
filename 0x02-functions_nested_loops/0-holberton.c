d26149e44247d3c28d56eb47c15d700590ff7505
Solienne <soliene.teixeira@ufba.br>
2022-09-01 22:35:33 +0000

    prints Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *str = "Holberton";
+
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar ('\n');
+
+	return (0);
+}
