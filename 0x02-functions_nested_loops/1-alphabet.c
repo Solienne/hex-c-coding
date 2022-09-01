46983a93ac927877f33453c79324700ba873d14a
Solienne <soliene.teixeira@ufba.br>
2022-09-01 23:16:47 +0000

    printing alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet - print alphabet in lowercase
+ *
+ **/
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
