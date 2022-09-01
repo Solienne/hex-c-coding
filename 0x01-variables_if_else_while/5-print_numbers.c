995ef3c5fb5c359c5f9590418d359c4950a4e72d
Solienne <soliene.teixeira@ufba.br>
2022-09-01 01:26:16 +0000

    print single numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - prints single numbers os base ten < 110
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
