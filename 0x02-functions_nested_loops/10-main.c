5e5796bcff829549e10e857505e5c09f4b74c452
Solienne <soliene.teixeira@ufba.br>
2022-09-02 02:44:30 +0000

    adds 2 numbers

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,17 @@
+/**
+* main - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
+
+#include "holberton.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
+}
