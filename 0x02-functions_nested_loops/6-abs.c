f8247fb861743d213465f6bbb42692fe2e67423e
Solienne <soliene.teixeira@ufba.br>
2022-09-02 02:14:18 +0000

    prints the absolut value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _abs - prints the absolute value of an integer
+ * @n: The number which absolute value will be printed
+ * Return: The absolute value
+ */
+int _abs(int n)
+{
+	int a;
+
+	if (n < 0)
+	{
+		a = n * -1;
+
+		return (a);
+	}
+	else
+	{
+		return (n);
+	}
+}
