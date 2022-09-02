f155bc7c59a7ac7e44b366cd1d0fbb2c5037e532
Solienne <soliene.teixeira@ufba.br>
2022-09-02 22:07:13 +0000

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* main - checks if main.c gives the correct output
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative();
+
+	return (0);
+}
---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,8 @@
+#ifndef HOLLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif /* HOLBERTON_H */ 
