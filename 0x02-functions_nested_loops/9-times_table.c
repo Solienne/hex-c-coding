65dc8bcab7c20d2af2e6ca8bb13139049acba6aa
Solienne <soliene.teixeira@ufba.br>
2022-09-02 02:37:00 +0000

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * times_table - prints the 9 time tables, starting with 0
+ */
+void times_table(void)
+{
+	int p, n, m;
+
+	for (n = 0; n < 10; n++)
+	{
+		_putchar('0');
+
+		for (p = 1; p < 10; p++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			m = (p * n);
+		if (m < 10)
+			_putchar(' ');
+		else
+			_putchar((m / 10) + '0');
+
+		_putchar((m % 10) + '0');
+		}
+	_putchar('\n');
+	}
+}
