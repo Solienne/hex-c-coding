3ce6435caad663444b5c8b001131daa8c1e46f2d
Solienne <soliene.teixeira@ufba.br>
2022-09-04 20:12:40 +0000

    find digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* _isdigit - function that checks for digits from 0 through 9
+* @c: type int
+* Return: 1 if int c is 0 through 9, 0 if otherwise
+*/
+int _isdigit(int c)
+{
+	if (c>= '0' && c<= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
