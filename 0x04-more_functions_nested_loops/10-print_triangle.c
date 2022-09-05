b1e612345625e30899fd12b151db3978cccf0d40
Solienne <soliene.teixeira@ufba.br>
2022-09-05 02:16:00 +0000

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,25 +7,25 @@
 */
 void print_triangle(int size)
 {
-        int i, j, b;
+	int i, j, b;
 
-        if (size <= 0)
-        {
-                _putchar('\n');
-        }
-        else
-        {
-                for (i = 0; i < size; i++)
-                {
-                        for (j = 0; j < size; j++)
-                        {
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
 				b = size - i - 1;
 				if (j < b)
 					_putchar(' ');
 				else
-                                	_putchar(35);
-                        }
-                        _putchar('\n');
-                }
-        }
+					_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
 }
