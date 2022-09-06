d4eab9005add15b258a43d7ac6a84cc14b982ee1
Solienne <soliene.teixeira@ufba.br>
2022-09-06 00:08:59 +0000

    done!

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+* _strncat - function that concatenates two strings
+* @dest: A pointer to the string
+* @src: String to be appended
+* @n: concatenates caracters
+* Return: dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = j = 0;
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+	while (*(src + j) != '\0' && n > j)
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+* _strcmp - function that compares two strings.
+* @s1: first string
+* @s2: second string
+* Return: + if >, - if < and 0 if =
+*/
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 > *s2)
+		{
+			return (*s1 - *s2);
+		}
+		else if (*s1 < *s2)
+		{
+			return (*s1 - *s2);
+		}
+	s1++;
+	s2++;
+	}
+	return (0);
+}
